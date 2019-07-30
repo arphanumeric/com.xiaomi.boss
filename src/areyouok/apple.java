f923993fbd0b89fe6fcf64241567bd3002bd281e
user <meEmail@google.com>
2019-07-30 14:04:27 +0800

    test new

--- src/areyouok/apple.java
+++ src/areyouok/apple.java
@@ -3,7 +3,7 @@ package areyouok;
 public class apple {
 	public static void main(String[] args) {
 		System.out.println("are You ok");
-		System.out.println("are You ok");
+		System.out.println("are she ok");
 		System.out.println("are You ok");
 	}
 
